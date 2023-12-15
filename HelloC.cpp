b373b55ee52c3019b5bbc11357f0f72faa57a13f
NHATDUY119 <146502751+NHATDUY119@users.noreply.github.com>
2023-12-15 09:19:05 +0700

    Update HelloC.cpp

--- HelloC.cpp
+++ HelloC.cpp
@@ -1,10 +1,27 @@
-include <stdio.h>
-
-int main(){
-	
-	printf("\n Than chao cac ban DA23TTA");
-
-	printf("Day la thu kkhong can thiet");
-	
-	return 0;
+#include "stdio.h"
+#include "stdlib.h"
+I
+int main()
+(int
+int m=3;
+a[3][3]={{1,2,3},{4,5,6},{7,8,9)};
+FILE *fp;
+char path [50];
+printf("\nNhap dưỡng dan: ");
+fflush(stdin);
+gets (path);
+fp=fopen(path,"wt");
+if(fp==NULL)
 }
+else
+{
+}
+printf("\nLoi mo file");
+exit(0);
+fprintf(fp,"So dong, cot là %d\n",m);
+for (int i=0; i<m; i++)
+for (int j 0; j<n; j++)// ghi dong
+(
+fprintf(fp,"%3d", a[i][j]);
+fprintf(fp,"\n");
+fclose(fp);
