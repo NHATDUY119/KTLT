71d1d19b7a220052762a1c0adb5c6db3e67bf2bd
NHATDUY119 <146502751+NHATDUY119@users.noreply.github.com>
2023-12-20 07:58:18 +0700

    Update HelloC.cpp

--- HelloC.cpp
+++ HelloC.cpp
@@ -1,4 +1,4 @@
-#include<stdio.h>
+hy#include<stdio.h>
 #include<conio.h>
 #include<string.h>
 typedef struct
@@ -92,3 +92,20 @@ H
 printf("Tim khong thay sinh vien co ma %s",MSSV);
 printf("\nBam phim bat ky de tiep tuc!!!");
 getch();
+
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int fibonacci(int n) {
+if (n < 0) {
+return -1;
+} else if (n == 0 || n == 1) {
+return n;
+} else {
+return fibonacci(n - 1) + fibonacci(n - 2);
+int main() {
+int i;
+printf("10 so dau tien cua day so Fibonacci: \n");
+for (i = 0; i < 10; i++) {
+printf("%d" fibonacci(i));
