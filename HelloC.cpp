f4167cf7cc7622932f31de25bfa53c0a7850fb92
NHATDUY119 <146502751+NHATDUY119@users.noreply.github.com>
2023-12-20 08:20:23 +0700

    Update HelloC.cpp

--- HelloC.cpp
+++ HelloC.cpp
@@ -129,3 +129,28 @@ scanf("%d",&n);
 printf "Giai thua cua d la: %d\n"
 100021
 n, tinhGiaithua(n));
+
+
+
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int tongN(int n); I
+int main() {
+int i, n, tinhtongN;
+printf("Nhap mot so nguyen duong: ");
+scanf("%d", &n);
+tinhtongN
+tongN(n);
+, n, tinhtongN);
+printf("\nTong cac so tu 1 toi %d la: %d"
+return (0);
+=
+int tongN(int n) {
+int tinhtong;
+if (n == 1) {
+return (1);
+} else
+tinhtong = n + tongN(n - 1);
+return (tinhtong);
